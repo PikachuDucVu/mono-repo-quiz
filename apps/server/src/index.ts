4b0592460521464819dc5a2ce7a02d707e80674d
PikachuDucVu <ducvubn1@gmail.com>
2024-11-19 03:34:00 +0700

    remove cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,13 +8,13 @@ import AdminAPI from "./services/apis/AdminAPI";
 import { connectMongoose } from "./services/mongoose";
 
 const app = new Hono();
-app.use(
-  "*",
-  cors({
-    origin: [process.env.URL_ADMIN_APP, process.env.URL_CLIENT_APP],
-    credentials: true,
-  })
-);
+// app.use(
+//   "*",
+//   cors({
+//     origin: [process.env.URL_ADMIN_APP, process.env.URL_CLIENT_APP],
+//     credentials: true,
+//   })
+// );
 app.use(logger());
 
 const currentServerTime = new Date().toISOString();
